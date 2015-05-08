93ad2651de1e9869759e1a123c210cb9c890998e
alex_yat <sanyayatov@gmail.com>
2015-05-08 20:20:35 +0300

    change grunt file add build directories and add main_page

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    
+    $('.tab').hover(function(){
+        $('.tab').css({'width':'360px'});
+        $(this)
+            .addClass('active')
+            .css({'width':'560px', 'backgroundPosition': '-1040px 0'})
+            .siblings().css({'backgroundPosition': '-1238px 0'})
+            .removeClass('active');
+        $(this).find('span.text').addClass('active').removeClass('not_active');
+        $(this).siblings().find('span.text').removeClass('active').addClass('not_active');
+    });
+
+    $('.add_post .add').click(function () {
+        $('#write_post').slideToggle();
+    });
+
+    $('.search').keypress(function(e){
+        e.preventDefault();
+        if(e.keyCode==13){
+            $('.main_menu').hide();
+            $('.seacrh_items').show();
+        }
+    });
 });
